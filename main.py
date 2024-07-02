e4d4fc92e4a7fafd27e07aa71850eb1cb63c3ded
Alisonsantos77 <Alisondev77@hotmail.com>
2024-07-02 18:00:58 -0300

    Adicionada funcionalidade de troca de esquema de cores, ocultação da barra de título da janela, gerenciamento de diálogos e melhorias na interface do usuário.
    - Adicionado `icone_menu` como referência para o botão de menu.
    - Ocultada a barra de título da janela e os botões da barra de título com `page.window.title_bar_hidden` e `page.window.title_bar_buttons_hidden`.
    - Implementada a função `change_color_scheme` para permitir a troca do esquema de cores (vermelho, azul, verde) no menu de navegação.
    - Adicionada funcionalidade de confirmação de fechamento da janela com o diálogo `close_dialog` e suas funções associadas `confirm_close_window`, `cancel_close_window` e `close_window`.
    - Atualizada a barra de ferramentas (`appbar_menu`) com novos botões: `troca_tema`, `minimize_window`, e `confirm_close_window`.
    - Adicionado diálogo de suporte (`dlg`) com links para WhatsApp, email, LinkedIn e GitHub.
    - Melhorias gerais na interface do usuário, incluindo novos ícones e layout atualizado para a barra de ferramentas e menu de navegação.
    - Correções de bugs menores e melhorias na lógica de navegação e atualização da página.
    
    Alterações específicas:
    - Funções novas: `change_color_scheme`, `confirm_close_window`, `cancel_close_window`, `close_window`, `open_dialog`, `close_dialog`, `minimize_window`.
    - Atualização de `appbar_menu` para incluir áreas de arrasto da janela e novos botões.
    - Atualização de `menu` para incluir seleção de esquema de cores e botão de suporte.
    - Ajustes na lógica de `route_change` para garantir a atualização correta da página.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from pages.speed import Speed
 def main(page: ft.Page):
     # Refs
     icone_tema = ft.Ref[ft.IconButton]()
+    icone_menu = ft.Ref[ft.IconButton]()
+
+    page.window.title_bar_hidden = True
+    page.window.title_bar_buttons_hidden = True
 
     def change_route(e):
         match e.control.selected_index:
@@ -21,12 +25,127 @@ def main(page: ft.Page):
         menu.open = True
         page.update()
 
+    def troca_tema(e):
+        e.page.theme_mode = (
+            ft.ThemeMode.DARK if e.page.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
+        )
+        e.page.update()
+
+    def change_color_scheme(e):
+        selected_color = e.control.selected
+        if "red" in selected_color:
+            page.theme = ft.colors.RED
+            page.theme = ft.Theme(
+                color_scheme=ft.ColorScheme(primary=ft.colors.RED))
+        elif "blue" in selected_color:
+            page.theme = ft.colors.BLUE
+            page.theme = ft.Theme(
+                color_scheme=ft.ColorScheme(primary=ft.colors.BLUE))
+        elif "green" in selected_color:
+            page.theme = ft.colors.GREEN
+            page.theme = ft.Theme(
+                color_scheme=ft.ColorScheme(primary=ft.colors.GREEN))
+        page.update()
+
+    def confirm_close_window(e):
+        close_dialog.open = True
+        page.update()
+
+    def cancel_close_window(e):
+        close_dialog.open = False
+        page.update()
+
+    def close_window(e):
+        page.window.close()
+        page.update()
+
+    def open_dialog(e):
+        dlg.open = True
+        page.update()
+
+    def close_dialog(e):
+        dlg.open = False
+        page.update()
+
+    def minimize_window(e):
+        page.window.minimized = True
+        page.update()
+
+    dlg = ft.AlertDialog(
+        title=ft.Row(
+            [
+                ft.Text("Fale conosco", size=24,
+                        weight=ft.FontWeight.BOLD, expand=1),
+                ft.IconButton(
+                    icon=ft.icons.CLOSE,
+                    tooltip="Fechar",
+                    on_click=close_dialog,
+                    icon_color=ft.colors.RED,
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+        ),
+        actions=[
+            ft.IconButton(
+                content=ft.Image(src='images/whatsapp-logo.png',
+                                 width=40, height=40),
+                icon_color=ft.colors.GREEN,
+                tooltip="Abrir Whatsapp",
+                url="https://wa.link/oebrg2",
+                style=ft.ButtonStyle(
+                    overlay_color={"": ft.colors.TRANSPARENT,
+                                   "hovered": ft.colors.LIGHT_GREEN_200},
+                )
+            ),
+            ft.IconButton(
+                content=ft.Image(
+                    src='images/microsoft-outlook-logo.png', width=40, height=40),
+                icon_color=ft.colors.BLUE,
+                tooltip="Enviar Email",
+                url="mailto:Alisondev77@hotmail.com?subject=Feedback%20-%20MultiTools&body=Ol%C3%A1,"
+                    "%20equipe!%20Gostaria%20de%20fornecer%20feedback%20sobre%20o%20aplicativo%20MultiTools.%20["
+                    "Descreva%20o%20feedback].%20Obrigado("
+                    "a)%20pela%20oportunidade%20de%20compartilhar%20minha%20opini%C3%A3o.",
+                style=ft.ButtonStyle(
+                    overlay_color={"": ft.colors.TRANSPARENT,
+                                   "hovered": ft.colors.LIGHT_BLUE_200},
+                )
+            ),
+            ft.IconButton(
+                content=ft.Image(src='images/linkedin-logo.png',
+                                 width=40, height=40),
+                icon_color=ft.colors.BLUE,
+                tooltip="Acessar Linkedin",
+                url="www.linkedin.com/in/alisonsantosdev",
+                style=ft.ButtonStyle(
+                    overlay_color={"": ft.colors.TRANSPARENT,
+                                   "hovered": ft.colors.LIGHT_BLUE_200},
+                )
+            ),
+            ft.IconButton(
+                content=ft.Image(src='images/github-logo.png',
+                                 width=40, height=40),
+                icon_color=ft.colors.GREY,
+                tooltip="Acessar Github",
+                url="https://github.com/Alisonsantos77",
+                style=ft.ButtonStyle(
+                    overlay_color={"": ft.colors.TRANSPARENT,
+                                   "hovered": ft.colors.GREY_300},
+                )
+            ),
+        ],
+        actions_alignment=ft.MainAxisAlignment.SPACE_AROUND,
+    )
+
+    page.overlay.append(dlg)
+
     menu = ft.NavigationDrawer(
         controls=[
             ft.Container(
                 content=ft.Row(
                     [
-                        ft.Text("Multi Tools", size=30, weight=ft.FontWeight.W_900),
+                        ft.Text("Multi Tools", size=30,
+                                weight=ft.FontWeight.W_900),
                         ft.Icon(ft.icons.SETTINGS, size=30),
                     ],
                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
@@ -50,9 +169,54 @@ def main(page: ft.Page):
                 icon=ft.icons.WATER_DROP_OUTLINED,
                 selected_icon_content=ft.FilledButton(text="Abrir")  # 2
             ),
-            ft.Container(expand=True),
             ft.Divider(height=2),
-
+            ft.Container(
+                content=ft.Column(
+                    [
+                        ft.Text("Tema", size=18, weight=ft.FontWeight.W_600,
+                                color=ft.colors.GREY_700),
+                        ft.SegmentedButton(
+                            on_change=change_color_scheme,
+                            selected={"red"},
+                            segments=[
+                                ft.Segment(
+                                    value="red",
+                                    label=ft.Text("Red", color=ft.colors.RED),
+                                    icon=ft.Icon(ft.icons.PALETTE,
+                                                 color=ft.colors.RED)
+                                ),
+                                ft.Segment(
+                                    value="blue",
+                                    label=ft.Text(
+                                        "Blue", color=ft.colors.BLUE),
+                                    icon=ft.Icon(ft.icons.PALETTE,
+                                                 color=ft.colors.BLUE)
+                                ),
+                                ft.Segment(
+                                    value="green",
+                                    label=ft.Text(
+                                        "Green", color=ft.colors.GREEN),
+                                    icon=ft.Icon(ft.icons.PALETTE,
+                                                 color=ft.colors.GREEN)
+                                ),
+                            ],
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.START,
+                    spacing=5,
+                ),
+                padding=ft.padding.all(10),
+            ),
+            ft.Divider(height=2),
+            # Seção "Me Conheça Mais" e "Suporte"
+            ft.Container(
+                content=ft.ElevatedButton(
+                    "Fale com o suporte.",
+                    icon=ft.icons.HEADSET,
+                    on_click=open_dialog,
+                ),
+                padding=ft.padding.all(10),
+            ),
             ft.Container(
                 content=ft.Text(
                     "Desenvolvido por Alison Santos",
@@ -68,32 +232,77 @@ def main(page: ft.Page):
         on_change=change_route,
     )
 
-    def troca_tema(e):
-        e.page.theme_mode = (
-            ft.ThemeMode.DARK if e.page.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
-        )
-        if e.page.theme_mode == ft.ThemeMode.DARK:
-            icone_tema.current.icon = ft.icons.WB_SUNNY_SHARP
-            icone_tema.current.update()
-        else:
-            icone_tema.current.icon = ft.icons.DARK_MODE_SHARP
-            icone_tema.current.update()
-        e.page.update()
+    close_dialog = ft.AlertDialog(
+        content=ft.Row(
+            controls=[
+                ft.Icon(ft.icons.WARNING, color=ft.colors.RED, size=30),
+                ft.Text(
+                    value="Tem certeza que deseja fechar o aplicativo?",
+                    size=16,
+                    color=ft.colors.RED
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        actions=[
+            ft.TextButton(
+                "Certeza",
+                on_click=close_window,
+                style=ft.ButtonStyle(color=ft.colors.RED)
+            ),
+            ft.TextButton(
+                "Cancelar",
+                on_click=cancel_close_window,
+                style=ft.ButtonStyle(color=ft.colors.WHITE)
+            ),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+        elevation=10,
+    )
 
+    # Adicionando o diálogo à sobreposição da página
+    page.overlay.append(close_dialog)
+
+    # Atualizando o AppBar
     appbar_menu = ft.AppBar(
         toolbar_height=50,
-        bgcolor=ft.colors.TRANSPARENT,
         color=ft.colors.INVERSE_PRIMARY,
-        leading=ft.IconButton(ft.icons.MENU, on_click=abrir_menu),
-        actions=[
-            ft.IconButton(
-                ft.icons.WB_SUNNY_OUTLINED,
-                ref=icone_tema,
-                on_click=troca_tema,
-                icon_color=ft.colors.INVERSE_PRIMARY,
-                padding=ft.padding.only(right=10),
+        leading=ft.WindowDragArea(
+            ft.Container(
+                content=ft.IconButton(
+                    ref=icone_menu, icon=ft.icons.MENU, on_click=abrir_menu),
+                padding=ft.padding.all(10),
             )
-        ]
+        ),
+        actions=[
+            ft.WindowDragArea(
+                ft.Container(
+                    content=ft.Row(
+                        controls=[
+                            ft.IconButton(
+                                icon=ft.icons.DARK_MODE,
+                                ref=icone_tema,
+                                on_click=troca_tema,
+                                tooltip="Modo Escuro",
+                            ),
+                            ft.IconButton(
+                                icon=ft.icons.MINIMIZE,
+                                on_click=minimize_window,
+                                tooltip="Minimizar",
+                            ),
+                            ft.IconButton(
+                                icon=ft.icons.CLOSE,
+                                on_click=confirm_close_window,
+                                tooltip="Fechar",
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.END,  # Alinhamento à direita
+                    ),
+                    padding=ft.padding.symmetric(horizontal=10),
+                    expand=True,
+                )
+            ),
+        ],
     )
 
     def route_change(route):
@@ -103,8 +312,7 @@ def main(page: ft.Page):
                 route='/',
                 appbar=appbar_menu,
                 drawer=menu,
-                controls=
-                [
+                controls=[
                     Speed(page)
                 ],
             )
@@ -115,8 +323,7 @@ def main(page: ft.Page):
                     route='/weather',
                     appbar=appbar_menu,
                     drawer=menu,
-                    controls=
-                    [
+                    controls=[
                         Weather(page)
                     ],
                 )
@@ -127,8 +334,7 @@ def main(page: ft.Page):
                     route='/youtube',
                     appbar=appbar_menu,
                     drawer=menu,
-                    controls=
-                    [
+                    controls=[
                         Youtube(page)
                     ],
                 )
